264645cff33b9f13a699cbe6a60ed8b3e00c10c0
dantecatalfamo <dante.catalfamo@gmail.com>
2022-08-14 15:00:29 -0400

    Make sndio-zig easier to import to other projects

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ hdl = sio_open(...)
 sio_write(hdl, arr, 5)
 hdl.write(arr, 5)
 ```
+
+# Importing
+```zig
+const addSndio = @import("sndio-zig/build.zig").addSndio;
+
+pub fn build(b: *std.build.Builder) void {
+    [...]
+    const exe = b.addExecutable("example", "src/main.zig");
+    addSndio(exe);
+    [...]
+}
+```
--- build.zig
+++ build.zig
@@ -7,13 +7,21 @@ pub fn build(b: *std.build.Builder) void {
 
     const lib = b.addStaticLibrary("sndio", "src/sndio.zig");
     lib.setBuildMode(mode);
-    lib.linkSystemLibrary("sndio");
+    addSndio(lib);
     lib.install();
 
     const main_tests = b.addTest("src/sndio.zig");
     main_tests.setBuildMode(mode);
-    main_tests.linkSystemLibrary("sndio");
+    addSndio(main_tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+}
+
+pub fn addSndio(exe: *std.build.LibExeObjStep) void {
+    var allocator = exe.builder.allocator;
+    const src_dir = std.fs.path.dirname(@src().file) orelse ".";
+    const package_path = std.fs.path.join(allocator, &.{ src_dir, "src", "sndio.zig" }) catch unreachable;
+    exe.linkSystemLibrary("sndio");
+    exe.addPackagePath("sndio", package_path);
 }
